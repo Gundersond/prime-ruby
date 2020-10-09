68c1604df22110f8b0ffb9aa8b7764edbf806455
Gundersond <dalton.gunderson@hotmail.com>
2020-10-08 20:44:40 -0500

    done

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
-# Add  code here!
+def prime?(number)
+    i = 2
+    if number.abs <= 1
+       return false
+    elsif number.abs == 2
+        return true
+    end
+    while number.abs > i
+        if number % i == 0
+            return false
+        end
+        i += 1
+    end
+    true
+end
